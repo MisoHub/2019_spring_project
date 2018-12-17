9ff8d9b5d8badda2a76f189dffe3ac62907c0dee
DESKTOP-PREEAR2\Desmond <wantkps@gmail.com>
2018-12-18 00:44:14 +0900

    chapter02 - envconfigbean(writting....2)

--- src/main/java/org/paulkim/chapter2/envConfigBean/EnvConfigBeanMain.java
+++ src/main/java/org/paulkim/chapter2/envConfigBean/EnvConfigBeanMain.java
@@ -3,6 +3,7 @@ package org.paulkim.chapter2.envConfigBean;
 import java.io.IOException;
 
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 //import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
@@ -14,11 +15,13 @@ public class EnvConfigBeanMain {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
-		ConfigurableApplicationContext context = new GenericXmlApplicationContext("classpath:**/envConfigBean.xml");
-		ConfigurableEnvironment configEnv = context.getEnvironment();
+		// set environment with properties
+		ConfigurableApplicationContext context = new GenericXmlApplicationContext();
+		ConfigurableEnvironment env = context.getEnvironment();
+		env.getPropertySources().addLast(new ResourcePropertySource("classpath:envconfigbean.properties"));
 		
-		MutablePropertySources propertySource = configEnv.getPropertySources();
-		propertySource.addLast(new ResourcePropertySource("classpath:**/envconfigbean.properties"));
+		context.getResource("classpath:**/envconfigbean.xml");
+		context.refresh();
 		
 		UserDefineBean messagePrinter = context.getBean("userMessagePrinter1",UserDefineBeanImpl.class);
 		
